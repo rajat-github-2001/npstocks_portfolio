a686b4cd5877239e35527def8ce62a4d35eb6ea2
rajat-github-2001 <rajat.joshi.smtp@gmail.com>
2024-01-28 11:09:30 +0545

    eslint rules updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,13 +13,22 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-    'semi': ['error', 'always'],
-    'no-console': 'warn',
-  },
-}
+    'strict': 0,
+    'linebreak-style': 0,
+    'indent': ["off", 4, { "SwitchCase": 1 }],
+    'react/jsx-indent': [0, 4],
+    'react/sort-comp': [0],
+    'react/jsx-no-target-blank': [0],
+    'react/no-unused-prop-types': [0],
+    'react/jsx-indent-props': [2, 4],
+    'react/jsx-filename-extension': ["error", {"extensions": [".js", ".jsx"] }],
+    'react/no-unknown-property': [0, { "ignore": ["styleName"] }],
+    'import/extensions': ["off", "never"],
+    'import/no-extraneous-dependencies': ["error", {"devDependencies": true }],
+    'react/forbid-prop-types': [1],
+    'react/prop-types': [0, { "ignore": [], "customValidators": [], "skipUndeclared": false }],
+    'no-unused-vars': [0, { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    'no-console': 0,
+    'no-mixed-operators': 0,
+  }
+};
